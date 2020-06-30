5d0033706275d66dbe6b4c83a1be5027955ef6f0
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-06-30 14:42:33 -0400

    updated static.json to include proies and headers

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ const PORT = process.env.PORT || 8080;
 
 // todo is cors necessary
 app.use(cors());
-const cors = require('cors');
-const corsOptions = {
-  origin: 'https://when-works.herokuapp.com/api/create',
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-app.options('*', cors());
 
 // Add headers
 // app.use(function (req, res, next) {
