e98de90775412b9415f4dcd57e1375ba3e5be5d0
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-06-30 13:51:16 -0400

    updated static.json to include proxy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
-const cors = require("cors");
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
