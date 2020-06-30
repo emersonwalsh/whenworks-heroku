e49077966e37e0e7e7f3b3b52c5505f1a235fedd
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-06-30 01:02:45 -0400

    added cors

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 
 const routes = require('./routes/api');
 
