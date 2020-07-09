0b6144c8ea3b9a96d67b6cabb28c93e6af04d740
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-07-09 19:53:55 -0400

    home page styling

--- whenworks/src/components/Home/home.js
+++ whenworks/src/components/Home/home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { updateDocumentTitle } from './../../util';
 
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-import { updateDocumentTitle } from './../../util';
-
+// import Link from '@material-ui/core/Link';
 
 export default function Home(props) {
     let history = useHistory();
+
     updateDocumentTitle('WhenWorks');
+
+    // const preventDefault = (event) => event.preventDefault();
+
 
     return (
         <div className="app-content">
@@ -16,16 +20,24 @@ export default function Home(props) {
                 <h1 className="home__title">
                     WhenWorks
                 </h1>
-                <p className="home__tagline">Easily find a date that works for everyone!</p>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    endIcon={<AddIcon />}
-                    onClick={() => history.push('/create')}
-                >
-                    Create Event
-                </Button>
+                <p className="home__tagline">Find a date that works for everyone!</p>
+                <div className="home__create">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        endIcon={<AddIcon />}
+                        onClick={() => history.push('/create')}
+                    >
+                        Create Event
+                    </Button>
+                </div>
+                {/* TODO using local storage, save events for user (event name and id) */}
+                {/* <div className="home__link">
+                    <Link href="#" onClick={preventDefault}>
+                        Existing Events
+                    </Link>
+                </div> */}
             </div>
         </div>
     );
