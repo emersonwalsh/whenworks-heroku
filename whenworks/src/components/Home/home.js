b04ebf6538d178bca51286c94c68fa1fdd2ba309
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-07-09 20:37:13 -0400

    added logo to homepage

--- whenworks/src/components/Home/home.js
+++ whenworks/src/components/Home/home.js
@@ -4,6 +4,7 @@ import { updateDocumentTitle } from './../../util';
 
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 // import Link from '@material-ui/core/Link';
 
 export default function Home(props) {
@@ -13,13 +14,15 @@ export default function Home(props) {
 
     // const preventDefault = (event) => event.preventDefault();
 
-
     return (
         <div className="app-content">
             <div className="home__container">
-                <h1 className="home__title">
-                    WhenWorks
-                </h1>
+                <div className="home__title">
+                    <div className="home__title__logo">
+                        <EventAvailableIcon color="primary" style={{ fontSize: 36 }} />
+                    </div>
+                    <h1 className="home__title__text">WhenWorks</h1>
+                </div>
                 <p className="home__tagline">Find a date that works for everyone!</p>
                 <div className="home__create">
                     <Button
@@ -32,7 +35,10 @@ export default function Home(props) {
                         Create Event
                     </Button>
                 </div>
-                {/* TODO using local storage, save events for user (event name and id) */}
+                {/* 
+                TODO using local storage, save events for user (event name and id) 
+                { eventId: eventName, event2Id: event2Name }
+                */}
                 {/* <div className="home__link">
                     <Link href="#" onClick={preventDefault}>
                         Existing Events
